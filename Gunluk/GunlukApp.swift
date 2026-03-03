339948b61971b4825a95de88b8d5cfe9fd38a12b
Alper <alpersogut@icloud.com>
2026-03-03 15:35:23 +0300

    Initial Commit

---
+++ Gunluk/GunlukApp.swift
@@ -0,0 +1,17 @@
+//
+//  GunlukApp.swift
+//  Gunluk
+//
+//  Created by Alper Söğüt on 3.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct GunlukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
